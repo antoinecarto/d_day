fbf61e0fe3110a35bdcd77fe5b5f867840b8f065
antoine <antoine.dubost@gmail.com>
2025-08-12 12:31:38 +0200

    correction dans le help

--- .history/src/components/CalendarView_20250812122939.vue
+++ .history/src/components/CalendarView_20250812122939.vue
@@ -19,7 +19,7 @@
       <!-- Modal d'aide -->
       <div
         v-if="showHelp"
-        class="bg-white rounded-2xl max-w-2xl w-full max-h-[90vh] overflow-y-auto shadow-lg"
+        class="fixed inset-0 bg-black bg-opacity-100 flex items-center justify-center z-50 p-4"
         @click="showHelp = false"
       >
         <div class="bg-white rounded-2xl max-w-2xl w-full max-h-[90vh] overflow-y-auto" @click.stop>
